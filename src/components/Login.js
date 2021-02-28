c13da79b7eb14fa79f7c75186867f0ac6ce2cff1
yuliya_sharapa <juliya.sharapa@gmail.com>
2021-02-28 14:24:29 -0300

    add user registration (frontend)

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Login() {
     return (
-        <div>
-            login
+        <div className="container">
+          
+          <div className="mt-5 p-5 mx-auto bg-light rounded" style={{width: 400}}>
+            <form >
+                <h2 className="text-center mb-3">Log in</h2>       
+                <div className="form-group">
+                  <input 
+                      className="form-control"
+                      
+                      name="email"
+                      type="text"
+                      placeholder="Email"
+                      />
+                    
+                </div>
+                <div class="form-group">
+                    <input 
+                      className="form-control"
+                      
+                      name="password"
+                      type="password"
+                      placeholder="Password"
+                    />
+                    
+                </div>
+                <div class="form-group">
+                    <button type="submit" className="btn btn-primary btn-block font-weight-bold">Log in</button>
+                </div>    
+            </form>
+            <span>Have an account?</span><Link to="/register" className="col-1">Register</Link>
         </div>
-    )
-}
+        </div>
+      );
+    };
+
 
 export default Login
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,77 @@
-import React from 'react'
+import axios from 'axios';
+import React from 'react';
+import {useForm} from 'react-hook-form';
 
 function Register() {
+
+    const {register, handleSubmit} = useForm();
+
+    const onSubmit = async (data)=>{
+        console.log(JSON.stringify(data));
+        try {
+            const registeredData = data;
+            await axios.post('http://localhost:5000/auth/', registeredData)
+        } catch (error) {
+            console.error(error)
+        }
+    };
+
+    
+
     return (
-        <div>
-            register
+        
+    <form onSubmit={handleSubmit(onSubmit)} className="mt-3 p-5 mx-auto bg-light rounded" style={{width: 400}}>
+        <h2 className="text-center mb-3">Register</h2> 
+        <div className="form-group">
+            <input
+                className="form-control"
+                ref={register }
+                placeholder="Name"
+                name="name"
+                type="text"
+            />
+        
         </div>
-    )
+        
+        <div className="form-group">
+        <input
+            className="form-control"
+            ref={register }
+            placeholder="Last name"
+            name="lastName"
+            type="text"
+        />
+        
+        </div>
+
+        <div className="form-group">
+        <input
+            className="form-control"
+            ref={register }
+            placeholder="Email"
+            name="email"
+            type="text"
+        />
+        
+        </div>
+
+        <div className="form-group">
+        <input
+            className="form-control"
+            ref={register }
+            placeholder="Password"
+            name="password"
+            type="password"
+        />
+        
+        </div>
+
+        
+        <div className="form-group">
+        <button type="submit" className="btn btn-primary btn-block font-weight-bold">Submit</button>
+        </div>
+    </form>
+)
 }
 
 export default Register
